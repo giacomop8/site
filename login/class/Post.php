9a6f7e4f41ab2c330194340dd0d9a459ff733862
giacomop8 <giacomopv@outlook.com>
2022-11-08 11:07:09 -0300

    imagem corrigida

--- login/class/Post.php
+++ login/class/Post.php
@@ -9,110 +9,33 @@ class Post {
 
     public function createPost($title, $description, $text, $image) {
         include RAIZ ."/conexao.php";
-
         $this->title = $title;
         $this->description = $description;
-        $this->texto = $text;
-        
-        date_default_timezone_set('America/Sao_Paulo');
-        
+        $this->texto = $text;        
+        date_default_timezone_set('America/Sao_Paulo');        
         $title = ucfirst($title);
         $description = ucfirst($description);
         $text = ucfirst($text);
         $date = date('Y/m/d G:i');
 
-
-        /* inserindo dados na tabela posts */
-                    
+        /* inserindo dados na tabela posts */                    
         $insertPost = "INSERT INTO posts (title, description, text, date) VALUES ('$title', '$description', '$text', '$date')";
         mysqli_query($conexao, $insertPost); 
         mysqli_close($conexao);
 
         if($this->saveImage($image, $title, $date)) {
-            echo 'Salvo com sucesso';
+            echo 'Salvo com sucesso <br>';
+            print_r($image);
         }
         else {
             echo 'Erro ao salvar a imagem';
         }
-    }
-
-    public function editPost($id, $title, $description, $data, $text, $image) {
-        include RAIZ ."/conexao.php";
-        
-        date_default_timezone_set('America/Sao_Paulo');
-        
-        $title = ucfirst($title);
-        $description = ucfirst($description);
-        $text = ucfirst($text);
-        $date = date('Y/m/d G:i');
-
-        
-        if($this->editImage($id, $image, $title, $date)) {
-            echo 'Salvo com sucesso';
-            
-            $edit = "UPDATE posts SET title='$title', description='$description', text='$text', date='$data' WHERE id_post='$id'";
-            mysqli_query($conexao, $edit);
-            mysqli_close($conexao);
-        }
-        else {
-            echo '<br> Erro ao salvar a imagem';
-        }        
-    }
-
-    public function editImage($idPost, $image, $title, $date) {
-
-        /* se o input de imagem estiver vazio, fazer isso */
-        if($image['type'] == '') {
-            include RAIZ ."/conexao.php";
-
-            $selectImage = "SELECT * FROM images WHERE id_post='$idPost'";
-            $query = mysqli_query($conexao, $selectImage);
-            $image = mysqli_fetch_assoc($query);
-            $extension = $image['extension'];
-            $pathAntigo = $image['path'];
-            $name = $title;
-            $path = $_SERVER["DOCUMENT_ROOT"].'/projetos/site_raiz/uploads/posts/'.$name.'.'.$extension;
-
-            if(move_uploaded_file($pathAntigo, $path)) {
-                /* editando dados na tabela imagem, pegando id do ultimo post */
-                $editImage = "UPDATE images SET name_image='$name', extension='$extension', path='$path', date_image='$date', id_post=$idPost";
-                mysqli_query($conexao, $editImage);
-                mysqli_close($conexao);
-
-                return true;
-            }
-            else{
-                echo '<br> '. $pathAntigo;
-            }
-        }
-
-        /* caso contrario, faça isso */
-        else{
-            include RAIZ ."/conexao.php";
-
-            $temp = $image['tmp_name'];
-            $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
-            $name = $title;
-            $path = $_SERVER["DOCUMENT_ROOT"].'/projetos/site_raiz/uploads/posts/'.$name.'.'.$extension;
-
-            if(move_uploaded_file($temp, $path)) {
-
-                /* editando dados na tabela imagem, pegando id do ultimo post */
-                $editImage = "UPDATE images SET name_image='$title', extension='$extension', path='$path', date_image='$date', id_post=$idPost";
-                mysqli_query($conexao, $editImage);
-                mysqli_close($conexao);
-
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-    }
+    }    
 
     public function saveImage($image, $title, $date) {
         $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
         $temp = $image['tmp_name'];
+        $size = $image['size'];
         $name = $title;
         $path = $_SERVER["DOCUMENT_ROOT"].'/projetos/site_raiz/uploads/posts/'.$name.'.'.$extension;
 
@@ -133,7 +56,7 @@ class Post {
             $id = $lastPost['id_post'];
 
             /* inserindo dados na tabela imagem, pegando id do ultimo post */
-            $insertImage = "INSERT INTO images (name_image, extension, path, date_image, id_post) VALUES ('$title', '$extension', '$path', '$date', $id)";
+            $insertImage = "INSERT INTO images (name_image, extension, path, size, date_image, id_post) VALUES ('$title', '$extension', '$path', '$size', '$date', '$id')";
             mysqli_query($conexao, $insertImage);
             mysqli_close($conexao);
 
@@ -141,6 +64,73 @@ class Post {
         }
         else {
             return false;
+        }
+    }
+
+    public function editPost($id, $title, $description, $data, $text, $image) {
+        include RAIZ ."/conexao.php";        
+        date_default_timezone_set('America/Sao_Paulo');        
+        $title = ucfirst($title);
+        $description = ucfirst($description);
+        $text = ucfirst($text);
+        $date = date('Y/m/d G:i');
+        
+        if($this->editImage($id, $image, $title, $date)) {
+            echo 'Salvo com sucesso';            
+            $edit = "UPDATE posts SET title='$title', description='$description', text='$text', date='$data' WHERE id_post='$id'";
+            mysqli_query($conexao, $edit);
+            mysqli_close($conexao);
+        }
+        else {
+            echo '<br> Erro ao salvar a imagem';
+        }        
+    }
+
+    public function editImage($idPost, $image, $title, $date) {
+        
+        /* se o input de imagem estiver vazio, fazer isso */
+        if($image['type'] == '') {
+            include RAIZ ."/conexao.php";            
+            $selectImage = "SELECT * FROM images WHERE id_post='$idPost'";
+            $query = mysqli_query($conexao, $selectImage);            
+            $imageBd = mysqli_fetch_assoc($query);
+            $nameAntigo = $imageBd['name_image'];
+            $extension = $imageBd['extension'];
+            $pathAntigo = $imageBd['path'];
+            $size = $imageBd['size'];
+            $path = $_SERVER["DOCUMENT_ROOT"].'/projetos/site_raiz/uploads/posts/'.$title.'.'.$extension;            
+
+            if(copy($pathAntigo, $path)) {
+                /* editando dados na tabela imagem, pegando id do ultimo post */
+                $editImage = "UPDATE images SET name_image='$title', extension='$extension', path='$path', size='$size', date_image='$date', id_post=$idPost";
+                mysqli_query($conexao, $editImage);
+                mysqli_close($conexao);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        /* caso contrario, faça isso */
+        else{
+            include RAIZ ."/conexao.php";
+            $temp = $image['tmp_name'];
+            $size = $image['size'];
+            $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+            $name = $title;
+            $path = $_SERVER["DOCUMENT_ROOT"].'/projetos/site_raiz/uploads/posts/'.$name.'.'.$extension;
+
+            if(move_uploaded_file($temp, $path)) {
+                /* editando dados na tabela imagem, pegando id do ultimo post */
+                $editImage = "UPDATE images SET name_image='$title', extension='$extension', path='$path', size='$size', date_image='$date', id_post=$idPost";
+                mysqli_query($conexao, $editImage);
+                mysqli_close($conexao);
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 }
